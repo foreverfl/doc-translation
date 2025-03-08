e9fa3993cde7d7bcb5a2bd573de717b4d62e1643
foreverfl <forever_fl@naver.com>
2025-03-08 17:30:34 +0900

    Separate scripts using folder

---
+++ scripts/translate-file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# Create the logs directory if it doesn't exist
+mkdir -p logs
+
+# Set the log file path with a timestamp
+LOG_FILE="logs/translate-folder-$(date +"%Y-%m-%d_%H-%M-%S").log"
+
+# Execute the command while saving the output to the log file
+npm run translate-file "$@" 2>&1 | tee "$LOG_FILE"
